4eb056d6bcc6b4158e73c7a6438f13978c232fb4
dknoma <djknoma@gmail.com>
2019-09-21 15:16:14 -0700

    added folders and models to backend

--- api/app.js
+++ api/app.js
@@ -1,41 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const config = require('./server/config/config');
 
-var app = express();
+// the express app
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+// log requests to console
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.set('superSecret', config.secret);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// appending app to the require allows the express app to have access to the routes
+require('./server/routes')(app);
+// catch-all route where other paths get routed to that are not specified
+app.get('*', (req, res) => res.status(200).send({
+	message: '{ message: "HELLO THERE" }',
+}));
 
 module.exports = app;
