5788aaaa80a6d23c4ae02c953754e78eb48f65e7
Jordan <jaldujaili@ucdavis.edu>
2019-09-25 19:01:34 -0700

    feature/favorites: add migration and model

---
+++ api/models/favorite.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Favorite = sequelize.define('Favorite', {
+    userId: DataTypes.INTEGER
+  }, {});
+  Favorite.associate = function(models) {
+    // associations can be defined here
+  };
+  return Favorite;
+};
