6764b15046f13fcb73c6b63d0fb659c25fd2a136
dknoma <djknoma@gmail.com>
2019-10-03 20:08:35 -0700

    get token from backend

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -8,35 +8,69 @@ import useStyles from "./navbarstyle";
 export function Navbar() {
     const classes = useStyles();
     const token = localStorage.getItem('token');
-    console.log(`token ${token}`);
+    console.log(`token=${token}`);
 
-    return(
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography className={classes.title}>
+    if(token !== null) {
+      return (
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon/>
+              </IconButton>
+              <Typography className={classes.title}>
+                <Button
+                  href={'/home'}
+                  color="inherit"
+                >
+                  <Typography component={'span'} variant="h6" className={classes.title}>
+                    Meal Planner
+                  </Typography>
+                </Button>
+              </Typography>
               <Button
-                href={'/home'}
+                href={'/'}
+                className={classes.auth}
+                color="inherit"
+                onClick={() => {
+                  localStorage.removeItem('token');
+                }}
+              >
+                Logout
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+      )
+    } else {
+      return (
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon/>
+              </IconButton>
+              <Typography className={classes.title}>
+                <Button
+                  href={'/home'}
+                  color="inherit"
+                >
+                  <Typography component={'span'} variant="h6" className={classes.title}>
+                    Meal Planner
+                  </Typography>
+                </Button>
+              </Typography>
+              <Button
+                href={'/register'}
+                className={classes.auth}
                 color="inherit"
               >
-                <Typography component={'span'} variant="h6" className={classes.title}>
-                  Meal Planner
-                </Typography>
+                Sign up
               </Button>
-            </Typography>
-            <Button
-              href={'/register'}
-              className={classes.auth}
-              color="inherit"
-            >
-              Login
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    )
+            </Toolbar>
+          </AppBar>
+        </div>
+      )
+    }
 }
 export default Navbar;
