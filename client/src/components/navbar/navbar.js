ca30cd053a8ef6cacd8f285c8a38902b636679e3
dknoma <djknoma@gmail.com>
2019-10-21 23:09:20 -0700

    updated navbar to have a left drawer to pull up links to other parts of the site

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,21 +1,48 @@
 import React from 'react';
 
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
-
 import useStyles from "./navbarstyle";
+import ChevronLeftIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import {FaHeart} from "react-icons/fa";
 
 export function Navbar(props) {
     const classes = useStyles();
     const authorized = props.auth;
     console.log(`logged in=${authorized}`);
 
+    const [open, setOpen] = React.useState(false);
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };
+
     if(authorized) {
       return (
         <div className={classes.root}>
           <AppBar position="static">
             <Toolbar>
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <IconButton
+                color="inherit"
+                aria-label="menu"
+                onClick={handleDrawerOpen}
+                edge="start"
+                className={classes.menuButton}>
                 <MenuIcon/>
               </IconButton>
               <Typography className={classes.title}>
@@ -40,6 +67,38 @@ export function Navbar(props) {
               </Button>
             </Toolbar>
           </AppBar>
+          <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.drawerHeader}>
+              <IconButton onClick={handleDrawerClose}>
+                {classes.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              <Button
+                href={'/favorites'}
+                color="inherit"
+              >
+                <ListItem button key={'Favorites'}>
+                  <ListItemIcon>
+                    <span className="icon toggled heart">
+                      <FaHeart size ={25}/>
+                    </span>
+                  </ListItemIcon>
+                  <ListItemText primary={'Favorites'} />
+                </ListItem>
+              </Button>
+            </List>
+            {/*<Divider />*/}
+          </Drawer>
         </div>
       )
     } else {
@@ -76,6 +135,33 @@ export function Navbar(props) {
               </Button>
             </Toolbar>
           </AppBar>
+          <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.drawerHeader}>
+              <IconButton onClick={handleDrawerClose}>
+                {classes.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              <ListItem button key={'Favorites'}>
+                <ListItemIcon>
+              <span className="icon toggled heart">
+                <FaHeart size ={25}/>
+              </span>
+                </ListItemIcon>
+                <ListItemText primary={'Favorites'} />
+              </ListItem>
+            </List>
+            {/*<Divider />*/}
+          </Drawer>
         </div>
       )
     }
