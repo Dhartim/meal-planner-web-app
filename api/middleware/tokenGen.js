6bbb21aa1b608241a67b02b9200da5c399c49b02
dknoma <djknoma@gmail.com>
2019-10-28 15:23:23 -0700

    fixed sign up bug potentially

--- api/middleware/tokenGen.js
+++ api/middleware/tokenGen.js
@@ -8,10 +8,13 @@ module.exports = (res, user) => {
     email: user.email,
   };
   try {
-    return jwt.sign(payload, config.jwt.jwtSecret, {
+    const token = jwt.sign(payload, config.jwt.jwtSecret, {
       expiresIn: config.jwt.jwtDuration, // expires after 24 hours
     });
+    console.log("token gen - token = %s", token);
+    return token;
   } catch (Error) {
+    console.log("Token gen error.");
     return res.status(400).send({
       message: 'Unable to get token.',
     });
