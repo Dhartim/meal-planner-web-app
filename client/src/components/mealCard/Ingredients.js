194f674ba6487d8ec846b283564dbd347ac3ac29
Dharti Madeka <dpmadeka@dons.usfca.edu>
2019-11-06 18:26:39 -0800

    some comments for development

--- client/src/components/mealCard/Ingredients.js
+++ client/src/components/mealCard/Ingredients.js
@@ -1,28 +1,33 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import {FaCartPlus} from "react-icons/fa";
 import './mealCard.css';
 
-function Ingredients(){
-    return(
-        <div className= "row">
-            <div className ="col-6 col-sm-6">
-                will pull ingredients from db once api is ready
-                {/* display ingredients here frrom db */}
+class Ingredients extends React.Component {
+    //get ingredients of each meal and display it 
+    // have button to buy ingredients
+    //show prices of ingredients with respect to their location
+        // ask to enter zip code // location
+        //display price of each ingredients
+    render(){
+        return(
+            <div className= "row">
+                <div className ="col-6 col-sm-6">
+                    {/* will pull ingredients from db once api is ready */}
+                    {/* display ingredients here frrom db */}
+                </div>
+                <div className ="col-6 col-sm-6">
+                    {/* pull quantity from db */}
+                    {/* it will have measurement or quantity of ingredients  */}
+                </div>
+                <div className ="col-12 col-sm-12">
+                    {/* connect this to an api which gives price and ingredients according to location */}
+                    <Button>Buy Ingredients</Button>
+                </div>
             </div>
-            <div className ="col-6 col-sm-6">
-                pull quantity from db
-                {/* it will have measurement or quantity of ingredients  */}
-            </div>
-            <div className ="col-12 col-sm-12">
-            {/* connect this button to amazon fresh api */}
-                <Button variant="success">
-                    <img className = "imageStyle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzMc3cMTCxTnwEOHmag0jHwyuVXNkcNluT9NjOjrAZtnS_LHax&s" alt="" />
-                    Buy Ingredients {/*need to have space between image and text*/}
-                </Button> 
-            </div>
-        </div>
-        
-    );
+            
+        );
+    }
 }
 
 export default Ingredients;
