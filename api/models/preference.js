d456f9d1e7d26854ec93c80f6046e3e6dc21cef1
Alex Thompson <awt1088@gmail.com>
2019-11-15 18:02:16 -0800

    working on connecting front and back end

--- api/models/preference.js
+++ api/models/preference.js
@@ -1,15 +1,18 @@
-module.exports = (sequelize, DataTypes => {
+module.exports = (sequelize, DataTypes) => {
     const Preferences = sequelize.define('UserMacro', {
         userId: {type: DataTypes.INTEGER, primaryKey: true},
+        diet: DataTypes.STRING,
         calories: DataTypes.INTEGER,
         fat: DataTypes.INTEGER,
         protein: DataTypes.INTEGER,
         carbs: DataTypes.INTEGER,
-        currentWeight: DataTypes.INTEGER,
-        desiredWeight: DataTypes.INTEGER
+        weight: DataTypes.INTEGER,
+        desiredWeight: DataTypes.INTEGER,
+        mealCount: DataTypes.INTEGER,
+        priceLimit: DataTypes.INTEGER
     }, {});
     Preferences.associate = function (models) {
         Preferences.belongsTo(models.User, { foreignKey: 'userId'})
     };
     return Preferences;
-})
+}
--- client/src/components/questionnaire/questionnaire.js
+++ client/src/components/questionnaire/questionnaire.js
@@ -4,7 +4,7 @@ import RadioGroup from '../subcomponents/radioGroup';
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 // import { Form, Button } from 'react-bootstrap'
-import Axios from "axios";
+import axios from "axios";
 import "./questionnaire.css";
 
 export class Questionnaire extends Component {
@@ -15,6 +15,34 @@ export class Questionnaire extends Component {
     }
   }
 
+  createPreferences = () => {
+      const {
+          diet,
+          calories,
+          fat,
+          protein,
+          carbs,
+          weight,
+          desiredWeight,
+          mealCount,
+          priceLimit
+      } = this.state;
+
+      return axios.post('/preferences', {
+          diet: diet,
+          calories: calories,
+          fat: fat,
+          protein: protein,
+          carbs: carbs,
+          weight: weight,
+          desiredWeight: desiredWeight,
+          mealCount: mealCount,
+          priceLimit: priceLimit
+      })
+          .catch(err => {
+              console.log(err)
+          })
+  }
 
   render() {
     return(
@@ -35,8 +63,8 @@ export class Questionnaire extends Component {
             </div>
             <div className="form-text__container">
               <div className="form-control">
-                <label htmlFor="macros">Macros</label>
-                <input type="number" id="macros" placeholder="Enter Calorie Goal"/>
+                <label htmlFor="calories">Calories</label>
+                <input type="number" id="calories" placeholder="Enter Calorie Goal"/>
               </div>
               <div className="form-control">
                 <label htmlFor="fat">Fat</label>
