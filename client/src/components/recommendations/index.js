1f189bae4f23aaa3ad46ba178173bffecc50e2fc
Dharti Madeka <dpmadeka@dons.usfca.edu>
2019-11-18 18:00:52 -0800

    basic structure for recommendation with similar formate as favourite

--- client/src/components/recommendations/index.js
+++ client/src/components/recommendations/index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-import Recommendations from './Recommendations';
-import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-import Recommendations from "./recommendations";
->>>>>>> c17da5d5f065d769313a2a1fa328f72636e4a87b
+import Recommendations from "./Recommendations";
 export default Recommendations;
