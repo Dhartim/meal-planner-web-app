6b5e6c0944ff52add279d8dd9c3a94fde8133a39
Alex Thompson <awt1088@gmail.com>
2019-11-25 17:32:05 -0800

    login page now looks like signup page

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,103 +1,123 @@
-import React, { Component } from "react";
-import { Form, Button } from 'react-bootstrap'
+import React, {Component} from "react";
+import {Form, Button, Image} from 'react-bootstrap'
 import Axios from "axios";
 
 import "./login.css";
 import TextField from "@material-ui/core/TextField";
 import {UserContext} from "../../context/usercontext";
+import {Box, Container, CssBaseline, Typography} from "@material-ui/core";
 
 export default class Login extends Component {
-  static contextType = UserContext;
+    static contextType = UserContext;
 
-  constructor(props) {
-    super(props);
-    this.onSubmit = this.onSubmit.bind(this);
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
 
-    this.state = {
-      email: '',
-      password: '',
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
     };
-  }
 
-  onSubmit = (e) => {
-    e.preventDefault();
-  };
+    tryLogin = () => {
+        // const EmailContext = React.createContext('email')
+        const {email, password} = this.state;
+        console.log(this.state)
+        Axios.post('/login', {
+            email: email,
+            password: password
+        })
+            .then((response) => {
+                console.log(response);
+                console.log(`token=${response.headers.token}`);
+                console.log(`status=${response.status}`);
 
-  tryLogin = () => {
-    // const EmailContext = React.createContext('email')
-    const { email, password } = this.state;
-    console.log(this.state)
-    Axios.post('/login', {
-      email: email,
-      password: password
-    })
-    .then((response) => {
-      console.log(response);
-      console.log(`token=${response.headers.token}`);
-      console.log(`status=${response.status}`);
+                if (response.status === 200) {
+                    const data = response.data;
+                    console.log("logged in user: %s, %s", data.userId, data.auth);
+                    this.context.changeUser(data.userId, data.auth);
 
-      if (response.status === 200) {
-        const data = response.data;
-        console.log("logged in user: %s, %s", data.userId, data.auth);
-        this.context.changeUser(data.userId, data.auth);
+                    console.log(`Got token`);
+                    localStorage.setItem('jwtToken', response.headers.token);
+                    this.props.history.push('/dashboard');
+                    // window.location.reload();
+                } else {
+                    console.log(`Must redirect`);
+                    // TODO: Redirect and display message
+                }
+            })
+            .catch(error => {
+                console.log("LOGIN - error status code: %s", error);
+                console.log(error);
+            });
+    };
 
-        console.log(`Got token`);
-        localStorage.setItem('jwtToken', response.headers.token);
-        this.props.history.push('/dashboard');
-        // window.location.reload();
-      } else {
-        console.log(`Must redirect`);
-        // TODO: Redirect and display message
-      }
-    })
-    .catch(error => {
-      console.log("LOGIN - error status code: %s", error);
-      console.log(error);
-    });
-  };
-  render() {
-    return (
-      <Form onSubmit={this.onSubmit}>
-        <Form.Group controlId="formBasicEmail">
-          <TextField
-            variant="outlined"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            value={this.state.email}
-            type={'email'}
-            autoComplete="email"
-            onChange={e => {
-              this.setState({ email: e.target.value });
-              this.setState({ validEmail: e.target.validity.valid });
-            }}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <TextField
-            variant="outlined"
-            required
-            fullWidth
-            name="password"
-            value={this.state.password}
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={e => {
-              this.setState({ password: e.target.value })
-            }}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={() => {
-          this.tryLogin();
-        }}>
-          Submit
-        </Button>
-        {/*<AccountProvider value={this.state}/>*/}
-      </Form>
-    );
-  }
+    render() {
+        return (
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <div className={"form-container"}>
+                    <center className={"center-elem"}>
+                        {/*<Avatar className={classes.avatar} >*/}
+                        {/*<FastfoodSharpIcon />*/}
+                        <Image className={"small-logo"} src={require("../../assets/images/full-logo.png")}/>
+                        {/*</Avatar>*/}
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                    </center>
+                    <Form onSubmit={this.onSubmit}>
+                        <Form.Group controlId="formBasicEmail">
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                value={this.state.email}
+                                type={'email'}
+                                autoComplete="email"
+                                onChange={e => {
+                                    this.setState({email: e.target.value});
+                                    this.setState({validEmail: e.target.validity.valid});
+                                }}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password"
+                                value={this.state.password}
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                onChange={e => {
+                                    this.setState({password: e.target.value})
+                                }}
+                            />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={() => {
+                            this.tryLogin();
+                        }}>
+                            Submit
+                        </Button>
+                        {/*<AccountProvider value={this.state}/>*/}
+                    </Form>
+                </div>
+                {/*<Box mt={5}>*/}
+                {/*    {this.copyright()}*/}
+                {/*</Box>*/}
+                <br/>
+            </Container>
+        );
+    }
 }
