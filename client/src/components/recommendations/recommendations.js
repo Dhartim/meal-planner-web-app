5ee802090f06d92ee81aadba9f3ee05f71963eda
Dharti Madeka <dpmadeka@dons.usfca.edu>
2019-11-30 11:58:24 -0800

    recommendation file

--- client/src/components/recommendations/recommendations.js
+++ client/src/components/recommendations/recommendations.js
@@ -1,68 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
 import MealCard from "../mealCard";
-import Slider from "react-slick";
-import './recommendations.css'
-export class Recommendations extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loggedIn: false,
-      recommendations: [],
-    };
-  }
-
-  componentDidMount(){
-    const jwtToken = localStorage.getItem('jwtToken');
-
-    axios
-      .get('/recommendations', {
-        headers: {"x-access-token" : `${jwtToken}`}
-      })
-      .then(res => {
-        this.setState({
-          recommendations: res.data,
-          loggedIn: true
-        })
-      })
-      .catch(error => error)
-      console.log("RECOMMENDATION = ", this.state.recommendations);
-  }
-
-  render() {
-    const { loggedIn, recommendations } = this.state;
-    let recommendationList = [];
-    var settings = {
-      dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4
-  };
-    if(loggedIn) {
-      for(let i = 0; i < recommendations.length; i++) {
-        let recommend = recommendations[i];
-        console.log("Recommendation =", recommend);
-        recommendationList.push( 
-            <
-              MealCard
-              key={recommend.id}
-              {...recommend}
-              cuisineType = {recommend.cuisineType}
-          />
-        )
-      }
-    } else {
+class Recommendations extends Component{
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          loggedIn: false,
+          recommendations: [],
+        };
     }
-    return(
-      <div className="recommend__row">
-          <Slider {...settings}>
-            {recommendationList}
-          </Slider>
-      </div>
-    );
-  }
-}
 
+    componentDidMount(){
+        const jwtToken = localStorage.getItem('jwtToken');
+    
+        axios
+          .get('/recommendations', {
+            headers: {"x-access-token" : `${jwtToken}`}
+          })
+          .then(res => {
+            this.setState({
+              recommendations: res.data,
+              loggedIn: true
+            })
+          })
+          .catch(error => error)
+    }
+    //recommendation will have column with slider but it will have recommendated meals as per
+    // questionnaire
+    // it will be topmost and fixed row for all users
+    render(){
+        const { loggedIn, recommendations } = this.state;
+        let recommendationList = [];
+        if(loggedIn) {
+            // for each favorited meal, render a new meal card
+            for(let i = 0; i < recommendations.length; i++) {
+              let recommend = recommendations[i];
+              recommendationList.push(
+                <
+                  MealCard
+                  key={recommend.id}
+                  {...recommend}
+                  cuisineType = {recommend.cuisineType}
+                />
+              )
+            }
+          } else {
+      
+          }
+        return(
+            <div>
+                <h2>Recommendations</h2>
+                {recommendationList}
+            </div>
+        );
+    }
+}
 export default Recommendations;
