abac3cea0a1f1bdbcddec9202ce7bde9223aee36
Jordan <jaldujaili@ucdavis.edu>
2019-12-08 22:09:00 -0800

    fix/customer: fix stripe

--- client/src/components/accountPage/accountComponents/profileComponent/profileComponent.jsx
+++ client/src/components/accountPage/accountComponents/profileComponent/profileComponent.jsx
@@ -10,13 +10,16 @@ const ProfileComponent = props => {
       <div className="profile__image-container"/>
       {/* <img className={'centered-image'} src={require('./headshot.jpg')}/> */}
       <h3 className="profile__intro">
+        <p>
           Welcome back {state.firstName} {state.lastName}!
+        </p>
+        <br/>
+        <p>
+          Your current email is {state.email}
+        </p>
+        { message }
       </h3>
-      <br/>
-      {/* <p>
-      Your current email is {state.email}
-      </p> */}
-      {/* {props.message} */}
+      
     </div>
   )
 }
--- client/src/components/questionnaire/questionnaire.js
+++ client/src/components/questionnaire/questionnaire.js
@@ -56,9 +56,6 @@ export class Questionnaire extends Component {
             })
     }
 
-    
-
-
     handleChange (event) {
       this.setState({ [event.target.id]: event.target.value });
     }
